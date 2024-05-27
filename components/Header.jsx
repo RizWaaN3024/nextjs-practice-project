ab287ce2c711a1426501dba067276d41e8dfe6d8
Rizwaan <rizwaanans786@gmail.com>
2024-05-28 03:12:02 +0530

    First commit

---
+++ components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ThemeToggler from './ThemeToggler'
+
+const Header = () => {
+  return (
+    <header>
+      <ThemeToggler />
+    </header>
+  )
+}
+
+export default Header
---
+++ components/ThemeProvider.jsx
@@ -0,0 +1,10 @@
+"use client";
+
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+
+export function ThemeProvider({ children, ...props }) {
+    return <NextThemesProvider {...props}>
+        {children}
+        
+    </NextThemesProvider>
+}
