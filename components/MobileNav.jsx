355f252dca58bece1d22dc9561f2b4302cf74ffd
RizWaaN3024 <rizwaanans786@gmail.com>
2024-05-31 00:21:56 +0530

    Added Mobile Navigation

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
+import { Sheet, SheetContent, SheetTrigger, SHeetTrigger } from './ui/sheet'
+import { AlignJustify } from 'lucide-react'
+import Nav from './Nav'
+import Logo from './Logo'
+import Socials from './Socials'
 
 const MobileNav = () => {
   return (
-    <div>
-      
-    </div>
+    <Sheet>
+      <SheetTrigger asChild>
+        <AlignJustify className='cursor-pointer'/>
+      </SheetTrigger>
+      <SheetContent>
+        <div className='flex flex-col items-center justify-between h-full py-8'>
+          <div className='flex flex-col items-center gap-y-32'>
+            <Logo />
+            <Nav containerStyles={`flex flex-col items-center gap-y-6`} 
+              linkStyles={`text-2xl`}
+            />
+          </div>
+          <Socials containerStyles={`flex gap-x-4`}  iconStyles={`text-2xl`}/>
+        </div>
+      </SheetContent>
+    </Sheet>
   )
 }
 
