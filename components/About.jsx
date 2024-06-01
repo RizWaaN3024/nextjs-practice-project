f0ca4729f30836012a31002c5958fc2c8f187c8b
RizWaaN3024 <rizwaanans786@gmail.com>
2024-06-02 02:33:58 +0530

    Added Skills Section in about Section

--- components/About.jsx
+++ components/About.jsx
@@ -222,9 +222,46 @@ const About = () => {
                                         
                                     </div>
                             </TabsContent>
-                            
+                            {/* Skills */}
                             <TabsContent value="skills">
-                                skills Info
+                                <div className='text-center xl:text-left'>
+                                    <h3 className='h3 mb-8 '>What I Use Everyday</h3>
+                                    <div className='mb-16'>
+                                        <h4 className='text-xl font-semibold mb-2'>Skills</h4>
+                                        <div className='border-b border-border mb-4'></div>
+                                        {/* Skills List */}
+                                        <div>
+                                            {getData(skillData, 'skills').data.map((item, index) => {
+                                                const {name} = item;
+                                                return(
+                                                    <div className='w-2/4 text-center xl:text-left mx-auto xl:mx-0' key={index}>
+                                                        <div className='font-medium'>{name}</div>
+                                                    </div>
+                                                ) 
+                                            })}
+                                        </div>
+                                    </div>
+                                    {/* tools */}
+                                    <div>
+                                        <h4 className='text-xl font-semibold mb-2 xl:text-left'>Tools</h4>
+                                        <div className='border-b border-border mb-4'></div>
+                                        {/* Tools List */}
+                                        <div className='flex gap-x-8 justify-center xl:justify-start'>
+                                            {getData(skillData, 'tools').data.map((item, index) => {
+                                                const {imgPath} = item;
+                                                return (
+                                                <div key={index}>
+                                                    <Image src={imgPath} 
+                                                        width={48}
+                                                        height={48}
+                                                        alt=''
+                                                        priority
+                                                    />
+                                                </div>
+                                            )})}
+                                        </div>
+                                    </div>
+                                </div>
                             </TabsContent>
                         </div>
                     </Tabs>
