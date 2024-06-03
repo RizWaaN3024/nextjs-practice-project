10e0a1b3b997f90dcd5a8a83c8a55d5ce73b7b6c
RizWaaN3024 <rizwaanans786@gmail.com>
2024-06-04 00:51:49 +0530

    Added Footer Section

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import Socials from './Socials'
 
 const Footer = () => {
   return (
-    <div>
+    <footer className='bg-seconadry py-12'>
+      <div className='container mx-auto'> 
+      <div className='flex flex-col items-center justify-between'>
+        {/* socials */}
+        <Socials containerStyles={`flex gap-x-6 mx-auto xl:mx-0 mb-4`} iconStyles={`text-primary text-[20px] dark:text-white/70 hover:text-white dark:hover:text-primary transition-all`}/>
+        {/* copyright */}
+        <div className='text-muted-foreground'>
+          Copyright &copy; Ryan Davis. All rights reserved.
+        </div>
+      </div>
+
+      </div>
       
-    </div>
+    </footer>
   )
 }
 
