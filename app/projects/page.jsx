c67a58cfbc49c55cf30d8bd8ccccb9a7165dd51f
RizWaaN3024 <rizwaanans786@gmail.com>
2024-06-05 02:49:34 +0530

    Added My Projects Section

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState } from 'react'
-import { Tabs, TabsList, TabsContent, TabsTrigger } from '@radix-ui/react-tabs';
+import { Tabs, TabsList, TabsContent, TabsTrigger } from '@/components/ui/tabs';
 import ProjectCard from '@/components/ProjectCard';
 
 const projectData = [
@@ -85,14 +85,17 @@ const uniqueCategory = ['all projects', ...new Set(projectData.map((item) => ite
 const Projects = () => {
   const [categories, setCategories] = useState(uniqueCategory);
   const [category, setCategory] = useState('all projects');
-
+  const filteredProjects = projectData.filter(project => {
+    // if category is all projects return all projects
+    return category === 'all projects' ? project : project.category === category;
+  })
   return (
     <section className='min-h-screen pt-12'>
       <div className="container mx-auto">
         <h2 className='section-title mb-8 xl:mb-16 text-center mx-auto'>My Projects</h2>
         {/* tabs */}
-        <Tabs defaultValue={category}>
-          <TabsList className='w-full grid h-full md:grid-cols-4 lg:max-w-[640px] mb-12 mx-auto md:border dark:border-none'>
+        <Tabs defaultValue={category} className='mb-24 xl:mb-48'>
+          <TabsList className='w-full grid h-full md:grid-cols-4  lg:max-w-[640px] mb-12 mx-auto md:border dark:border-none'>
             {categories.map((category, index) => {
               return (
                 <TabsTrigger 
@@ -103,6 +106,16 @@ const Projects = () => {
               )
             })}
           </TabsList>
+          {/* Tabs Content */}
+          <div className='text-lg xl:mt-8 grid grid-cols-1 lg:grid-cols-3 gap-4'>
+            {filteredProjects.map((project, index) => {
+              return (
+                <TabsContent value={category} key={index}>
+                  <ProjectCard project={project}/>
+                </TabsContent>
+              )
+            })}
+          </div>
         </Tabs>
       </div>
     </section>
